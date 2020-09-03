9dfcd7775a1c10eeca2df20eb8cf1f5e9c239755
repl.it user <replituser@example.com>
2020-09-03 01:48:05 +0000

    second

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
 
@@ -9,21 +6,17 @@ var app = express();
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
+app.get('/', (req, res) => {
+  res.json({"ipaddress": req.header('x-forwarded-for'),
+            "language":  req.header('Accept-Language'),
+            "software":  req.header('User-Agent') });
+  // console.log(req.header('User-Agent'))
+  // console.log(req.header('x-forwarded-for'))
+  // console.log(req.header('Accept-Language'))
+})
 
 
 // listen for requests :)
