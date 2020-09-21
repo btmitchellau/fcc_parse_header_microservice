f5ad2b59d090bd39b87cd672f8fd91434570a1b6
btmitchell@hotmail.com <btmitchell@hotmail.com>
2020-09-21 14:31:59 +0930

    eslint changes

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
+const express = require('express');
+const cors = require('cors');
 
-var express = require('express');
-var app = express();
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionSuccessStatus: 200
+}));
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
-  res.json({"ipaddress": req.header('x-forwarded-for'),
-            "language":  req.header('Accept-Language'),
-            "software":  req.header('User-Agent') });
-  // console.log(req.header('User-Agent'))
-  // console.log(req.header('x-forwarded-for'))
-  // console.log(req.header('Accept-Language'))
-})
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  res.json({
+    ipaddress: req.header('x-forwarded-for'),
+    language: req.header('Accept-Language'),
+    software: req.header('User-Agent'),
+  });
 });
+
+app.listen(process.env.PORT);
